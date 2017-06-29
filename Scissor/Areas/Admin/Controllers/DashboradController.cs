2a04d83daeea69d7186e7552a61de3365cd36ad9
Administrator <manjulakp@hotmail.com>
2017-06-29 12:13:55 +0530

    admin dashboard

---
+++ Scissor/Areas/Admin/Controllers/DashboradController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scissor.Areas.Admin.Controllers
+{
+    public class DashboradController : Controller
+    {
+        // GET: Admin/Dashborad
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
